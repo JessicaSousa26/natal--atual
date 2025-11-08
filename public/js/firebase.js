cb7c2e2c32175fcb7d0f568139581ecf17a07cee
Jéssica Sousa <jhessymary26@gmail.com>
2025-11-08 18:57:45 -0300

    Atualizar configuração Firebase com nova API key e adicionar tratamento de erros no login

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDAW4ia7HeFUqvqwyD8DILe3EnclDzNdH8",
+  authDomain: "natal-votacao-vercel.firebaseapp.com",
+  projectId: "natal-votacao-vercel",
+  storageBucket: "natal-votacao-vercel.firebasestorage.app",
+  messagingSenderId: "316889485813",
+  appId: "1:316889485813:web:9bec2f269ebcbdb9f350bc",
+  measurementId: "G-5EGFMQ89ZX"
+};
+firebase.initializeApp(firebaseConfig);
