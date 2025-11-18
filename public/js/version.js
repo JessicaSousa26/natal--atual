5632a3fec07fbf4d9bad17637d17ffa83fdcebad
Jéssica Sousa <jhessymary26@gmail.com>
2025-11-17 23:40:08 -0300

    Adicionar patrocinador Wagner v1.4.2

--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Versão da aplicação - incrementar para forçar atualização
-const APP_VERSION = '1.4.1';
+const APP_VERSION = '1.4.2';
 
 // Sistema automático de atualização de cache
 (function() {
