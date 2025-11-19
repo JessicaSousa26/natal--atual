a4a5bb3c67717d578cfe2d2a591ad19b8d6c08cc
Jéssica Sousa <jhessymary26@gmail.com>
2025-11-18 22:27:45 -0300

    Adicionar crédito instalação elétrica WAGNER v1.6.1

--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Versão da aplicação - incrementar para forçar atualização
-const APP_VERSION = '1.6.0';
+const APP_VERSION = '1.6.1';
 
 // Sistema automático de atualização de cache
 (function() {
