18a457cb2a643b80fadfce50012d309e48dcf280
Jéssica Sousa <jhessymary26@gmail.com>
2025-11-18 22:18:42 -0300

    Fix: corrigir carrossel para ficar em 1 linha v1.5.9

--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Versão da aplicação - incrementar para forçar atualização
-const APP_VERSION = '1.5.8';
+const APP_VERSION = '1.5.9';
 
 // Sistema automático de atualização de cache
 (function() {
