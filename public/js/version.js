2aed3c4cb9bfa9b2cbddd4976a436da61814aec9
Jéssica Sousa <jhessymary26@gmail.com>
2025-11-18 22:07:44 -0300

    Fix: adicionar campos completos no backup de fotos v1.5.8

--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Versão da aplicação - incrementar para forçar atualização
-const APP_VERSION = '1.5.7';
+const APP_VERSION = '1.5.8';
 
 // Sistema automático de atualização de cache
 (function() {
