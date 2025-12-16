7b7679fd09a91843a2224993cc8519d6cc1a95d9
Jéssica Sousa <jhessymary26@gmail.com>
2025-12-16 15:33:07 -0300

    Admin uploads: merge updates, storage fix, local fallback and rules

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDAW4ia7HeFUqvqwyD8DILe3EnclDzNdH8",
   authDomain: "natal-votacao-vercel.firebaseapp.com",
   projectId: "natal-votacao-vercel",
-  storageBucket: "natal-votacao-vercel.firebasestorage.app",
+  storageBucket: "natal-votacao-vercel.appspot.com",
   messagingSenderId: "316889485813",
   appId: "1:316889485813:web:9bec2f269ebcbdb9f350bc",
   measurementId: "G-5EGFMQ89ZX"
